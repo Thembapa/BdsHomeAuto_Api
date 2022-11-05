6ce9f3aa1085d22e2750c77ee3e2f15b606c3f7b
Thembapa <36239015+Thembapa@users.noreply.github.com>
2022-11-05 23:29:51 +0200

    Signin api 50% done

--- notifications.py
+++ notifications.py
@@ -1,11 +1,14 @@
 from twilio.rest import Client 
+import smtplib
+from email.mime.text import MIMEText
+from email.message import EmailMessage
 import bdsconfig
 #Global Varibales
 account_sid = bdsconfig.account_sid
 auth_token = bdsconfig.auth_token
 client = Client(account_sid, auth_token) 
 
-#send msg 
+#send msg to sms list
 def sendsms(smsType,body,numbrs):
     if smsType==1:
         for number in numbrs:
@@ -16,3 +19,103 @@ def sendsms(smsType,body,numbrs):
                                     ) 
             #log sms to DB
             print(message.sid)
+
+#Send email to a mailing list
+def sendemail(mailingList,msg,file=None):
+    print(mailingList,msg)
+
+
+def sendmail(usubject, _msg, emaillist,_htmlBody =''):
+    server = smtplib.SMTP_SSL(configfile.EMAIL_SERVER, configfile.EMAIL_PORT)
+
+    # Next, log in to the server
+    server.login(configfile.EMAIL_USER, configfile.EMAIL_PASS)
+
+    # msg = MIMEText("""Hi, \n\n I think this will work. \n\n Regards""")
+    msg = EmailMessage()    
+    sender = configfile.EMAIL_USER
+    recipients = emaillist.split(",")
+    msg['Subject'] = usubject
+    msg['From'] = sender
+    msg['To'] = ", ".join(recipients)
+    msg.set_content(_msg)
+
+      
+    _html = """\
+       <!doctype html>
+
+            <html lang="en">
+            <head>
+            <meta charset="utf-8">
+            
+            <style>
+                .Header
+                {
+                    width: 100%;  
+                    height: 100px;   
+                    float: left;
+                }
+                .HeaderImage
+                {
+                    width: 100%; 
+                    height: 100px;
+                }
+                .btnStyle
+                {
+                    background-color: #be222f; /* Green */
+                    border: none;
+                    color: white;
+                    padding: 15px 32px;
+                    text-align: center;
+                    text-decoration: none;
+                    display: inline-block;
+                    position: relative;
+                    margin-left: 25%;
+                    font-size: 16px;
+                }
+                @media screen and (max-width: 767px) {
+                .HeaderImage
+                {
+                    height: 50px;
+                }
+                }
+                
+                
+            </style>
+            </head>
+
+            <body>
+            <div class="Header">
+                <img src="https://kasivore.com/images/Backgroud/Headerbackgroud.jpg" class="HeaderImage">
+            </div>
+            <div class="maindiv">
+                <div class="Content">
+                
+                <p>"""+_htmlBody + """
+                
+
+                </p>
+                </div>
+            </div>
+            
+                
+            </div>
+            </body>
+            </html>
+        """
+    if _htmlBody !='':
+        msg.add_alternative(_html, subtype= 'html')
+
+    server.sendmail(sender, recipients, msg.as_string())
+def sendWelcome_msg(email,tocken):
+    msg='''Hi,\n\n
+Thank you for signing up with BDS home Auto. Just few more steps and you are done.\n
+Please follow the link below to verify your email Address\n
+link:www.google.com\n\n
+tocken: {}
+
+NB: Your acount will only be active once you email is veryfied. \n
+The account will be deleted autmatically should the email not be verified withing 7 days
+    '''.format(tocken)
+    
+    sendemail(email,msg)
